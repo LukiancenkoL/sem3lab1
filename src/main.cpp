f86f2fc64ca12a84371ace20819afcc441efba62
LukiancenkoL <lzlukancenko@gmail.com>
2025-10-05 10:52:44 +0000

    commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,24 @@
 #include <stdio.h>
 #include <print>
 
+class A {
+public:
+	int x;
+};
+
+class B : public A {
+public:
+	float y;
+};
+
 int main(int argc, char **argv) {
-	std::println ("arg count: {}", argc);
+	B b;
+	b.y = 17.;
+	b.x = 3;
+	std::println("B fields: {}, {}", b.y, b.x);
+	std::println("arg count: {}", argc);
 	for (size_t i = 0; i < static_cast<size_t>(argc); i++) {
-		std::println("args: {}", argv[i]); 
+		std::println("args: {}", argv[i]);
 	}
 	return 0;
 }
